48455d905ecd0c3dbe2fe6cf55b1836fa68d47d4
gyull92 <wjsrbfuf@naver.com>
2025-11-15 18:16:05 +0900

    Feat: 아이콘 수정

--- electron/main.cjs
+++ electron/main.cjs
@@ -7,6 +7,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+     icon: path.join(__dirname, "..", "assets", "mandarinPDF3.ico"),
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       nodeIntegration: true,
